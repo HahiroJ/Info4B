b7d69daf6918b7a42d42ddf4b0c5f6a52b859537
Lucas <lhflyextra300@gmail.com>
2020-03-27 16:00:52 +0100

    grosse modif serveur

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -11,9 +11,10 @@ public class Server {
     static int port = 8080;
     static int maxClients = 10;
     static boolean help = false;
-    static PrintWriter pw[];
-    static int idClient;
-    static LinkedList<Player> clients = new LinkedList<Player>();
+    //static PrintWriter pw[];
+    static LinkedList<PrintWriter> pw;
+    //static int idClient;
+    static LinkedList<Player> clients;
 
     public static void main(String[] args) {
 
@@ -40,16 +41,17 @@ public class Server {
         }
 
         if (!help) {
-            pw = new PrintWriter[maxClients];
+            pw = new LinkedList<PrintWriter>();
+            clients = new LinkedList<Player>();
             try {
                 ServerSocket server_socket = new ServerSocket(port);
                 System.out.println("Socket listen => " + server_socket);
-                while (idClient < maxClients) {
+                while (clients.size() < maxClients) {
                     Socket socket = server_socket.accept();
-                    clients.add(new Player(idClient,socket));
-                    Thread client = new Thread(clients.get(idClient));
+                    clients.add(new Player(socket));
+                    Thread client = new Thread(clients.getLast());
                     System.out.println("Player is connected => " + socket);
-                    idClient++;
+                    //idClient++;
                     client.start();
                 }
             } catch (IOException e) {
@@ -62,6 +64,11 @@ public class Server {
             System.out.println("~~ CoreWar Game Server ~~");
         }
 
+    }
+
+    static synchronized public void remove(int i) {
+        pw.remove(i);
+        clients.remove(i);
     }
 
 }
